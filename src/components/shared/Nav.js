624af368afed12a959564df21876e849a9f754bd
mjp <peterson514@gmail.com>
2019-05-02 10:07:42 -0400

    Make sicky nav a generic component

---
+++ src/components/shared/Nav.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Grid } from '../grid/Grid';
+import { Link } from 'gatsby';
+import styles from './Nav.module.css';
+import Button from '../buttons/Button';
+
+export default ({ items, ctaText, ctaLink }) => (
+  <div className={styles.container}>
+    <Grid>
+      <div className={styles.navItems}>
+        {items.map((item, idx) => {
+          return (
+            <Link key={idx} to="/" className={idx === 0 ? styles.active : ''}>
+              <h6 className="small">{item.title}</h6>
+              <div className={styles.circle} />
+            </Link>
+          );
+        })}
+      </div>
+
+      {ctaText && (
+        <div className={styles.cta}>
+          <Button to={ctaLink}>{ctaText}</Button>
+        </div>
+      )}
+    </Grid>
+  </div>
+);
--- src/pages/deploy.js
+++ src/pages/deploy.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import AptibleLayout from '../components/layouts/AptibleLayout';
 import Hero from '../components/deploy/Hero';
-import Nav from '../components/deploy/Nav';
+import Nav from '../components/shared/Nav';
 import Features from '../components/deploy/Features';
 import SecurityControls from '../components/deploy/SecurityControls';
 import HowItWorks from '../components/deploy/HowItWorks';
@@ -10,11 +10,18 @@ import WhoUses from '../components/deploy/WhoUses';
 import Documentation from '../components/deploy/Documentation';
 import ZeroTo from '../components/footer/ZeroTo';
 
+const stickyNavItems = [
+  { title: 'Features', ref: null },
+  { title: 'How It Works', ref: null },
+  { title: 'Use Cases', ref: null },
+  { title: 'Who Uses Deploy', ref: null },
+  { title: 'Documentation & Support', ref: null },
+];
 
-export default ({ data }) => (
+export default () => (
   <AptibleLayout>
     <Hero />
-    <Nav />
+    <Nav items={stickyNavItems} ctaText="Start With Deploy" ctaLink="/" />
     <Features />
     <SecurityControls />
     <HowItWorks />
