1faaaf7617f83bc9cdcc50ac859057d76f6fa732
mjp <peterson514@gmail.com>
2019-05-02 14:06:55 -0400

    Nav resources drop down

--- src/components/header/Resources.js
+++ src/components/header/Resources.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import { Grid } from '../grid/Grid';
+import styles from './Resources.module.css';
 
 export default () => (
-  <Grid>
-    <p>Resources</p>
-  </Grid>
+  <div className={styles.container}>
+    <Grid>
+      <div className={styles.documentation}>
+        <Link to="/">Documentation</Link>
+        <Link to="/" className={styles.subLink}>Comply</Link>
+        <Link to="/" className={styles.subLink}>Deploy</Link>
+        <Link to="/" className={styles.subLink}>CLI</Link>
+      </div>
+      <div className={styles.blog}>
+        <Link to="/">Blog</Link>
+        <Link to="/" className={styles.subLink}>Product Updates</Link>
+        <Link to="/" className={styles.subLink}>Webinar Recap</Link>
+        <Link to="/" className={styles.subLink}>Newsletter</Link>
+      </div>
+      <div className={styles.guides}>
+        <h6 className="small">Compliance Guides</h6>
+        <Link to="/">HIPAA</Link>
+        <Link to="/">GDPR</Link>
+      </div>
+      <div className={styles.support}>
+        <h6 className="small">Support</h6>
+        <Link to="/">Open a Ticket</Link>
+        <Link to="/">Status</Link>
+      </div>
+    </Grid>
+  </div>
 );
