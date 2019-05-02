ff7675de9a969fcfdcd23577e02e129aaebb716b
mjp <peterson514@gmail.com>
2019-05-02 13:06:22 -0400

    Nav products drop down

--- src/components/header/Products.js
+++ src/components/header/Products.js
@@ -1,14 +1,94 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import { Grid } from '../grid/Grid';
+import styles from './Products.module.css';
+import complyBg from '../../images/illustrations/comply.png';
+import deployBg from '../../images/illustrations/deploy.png';
 
-export default () => (
-  <Grid>
-    <p>
-      <Link to="/comply">Comply</Link>
-    </p>
-    <p>
-      <Link to="/deploy">Deploy</Link>
-    </p>
-  </Grid>
-);
+const backgroundImages = {
+  comply: complyBg,
+  deploy: deployBg
+};
+
+
+function ComplyHover() {
+  return (
+    <div className={styles.hoverDescription}>
+      <h5>Design, operate, and audit your security management program.</h5>
+    </div>
+  )
+}
+
+function DeployHover() {
+  return (
+    <div className={styles.hoverDescription}>
+      <h5>Deploy audit-ready apps<br />and databases.</h5>
+    </div>
+  );
+}
+
+class Products extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openSectionName: null
+    };
+  }
+
+  setHover = (sectionName) => {
+    this.setState({ openSectionName: sectionName });
+  }
+
+  renderHoverContent = () => {
+    const content = {
+      comply: <ComplyHover />,
+      deploy: <DeployHover />
+    }
+
+    if (this.state.openSectionName in content) {
+      return content[this.state.openSectionName];
+    }
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <Grid>
+          <div className={styles.links}>
+            <Link
+              to="/comply/"
+              onMouseEnter={() => this.setHover('comply')}
+              onMouseLeave={() => this.setHover(null)}
+            >
+              <h3>Comply</h3>
+            </Link>
+
+            <Link
+              to="/deploy/"
+              onMouseEnter={() => this.setHover('deploy')}
+              onMouseLeave={() => this.setHover(null)}
+            >
+              <h3>Deploy</h3>
+            </Link>
+
+            <Link to="/pricing/">
+              <h3>Pricing</h3>
+            </Link>
+          </div>
+
+          <div className={styles.hoverContent}>
+            {this.renderHoverContent()}
+          </div>
+        </Grid>
+
+        {this.state.openSectionName && (
+          <div className={styles.hoverImage}>
+            <img src={backgroundImages[this.state.openSectionName]} alt="logo" />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Products;
