56087e404a40ef52e2494251c1fcdaf51bdb97b9
mjp <peterson514@gmail.com>
2019-05-17 11:01:23 -0400

    Button handles links and callbacks

--- src/components/buttons/Button.js
+++ src/components/buttons/Button.js
@@ -2,8 +2,20 @@ import React from 'react';
 import { Link } from 'gatsby';
 import styles from './Button.module.css';
 
-export default ({ to, theme, children }) => (
-  <Link to={to} className={`${styles.button} ${theme ? styles[theme] : ''}`}>
-    {children}
-  </Link>
-);
+export default ({ to, onClickFn, theme, children }) => {
+  const buttonStyle = `${styles.button} ${theme ? styles[theme] : ''}`;
+
+  if (to) {
+    return (
+      <Link to={to} className={buttonStyle}>
+        {children}
+      </Link>
+    )
+  } else if (onClickFn) {
+    return (
+      <div onClick={onClickFn} className={buttonStyle}>
+        {children}
+      </div>
+    )
+  }
+};
