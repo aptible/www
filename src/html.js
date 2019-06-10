33f8570cc52f51d58f6cf83aa3996b05083f5300
Michael Peterson <peterson514@gmail.com>
2019-06-10 16:02:40 -0400

    Analytics

--- src/html.js
+++ src/html.js
@@ -1,11 +1,19 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-const customJs = `
-  (function(w,d,t,u,n,s,e){w['SwiftypeObject'] = n; w[n]=w[n]||function(){
-  (w[n].q = w[n].q || []).push(arguments);};s=d.createElement(t);
-  e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
-  })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
+const GTMjs = `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T7S7ZTB');
+`;
+
+const segmentJs = `
+!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
+  analytics.load("rkt88i7k3w");
+  analytics.page();
+  }}();
 `;
 
 export default function HTML(props) {
@@ -19,11 +27,16 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
+        <script dangerouslySetInnerHTML={{ __html: GTMjs }}></script>
+        <script dangerouslySetInnerHTML={{ __html: segmentJs }}></script>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
         <noscript key="noscript" id="gatsby-noscript">
           This app works best with JavaScript enabled.
+        </noscript>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T7S7ZTB" height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
         </noscript>
         <div
           key={`body`}
@@ -31,12 +44,7 @@ export default function HTML(props) {
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
         {props.postBodyComponents}
-
       </body>
-
-      <script src="//app-ab35.marketo.com/js/forms2/js/forms2.min.js"></script>
-      <script src="https://js.chilipiper.com/marketing.js" type="text/javascript"></script>
-      <script dangerouslySetInnerHTML={{ __html: customJs }}></script>
     </html>
   )
 }
