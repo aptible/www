571bf452591e988e612227284fcf7bc01168b882
mjp <peterson514@gmail.com>
2019-06-09 21:13:43 -0400

    Embed customer videos into cards

--- src/components/shared/Card.js
+++ src/components/shared/Card.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 import styles from './Card.module.css';
 import Arrow from '../shared/Arrow';
+import WistiaVideo from './WistiaVideo';
 import videoPlayIcon from '../../images/video-play.svg';
 import customerCards from '../../data/customer-cards.json';
 
@@ -35,64 +36,97 @@ const logos = {
   virta: virtaLogo
 };
 
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showVideo: false
+    };
+  }
 
-export default ({ customer }) => {
-  const customerData = customerCards[customer];
+  videoClick = (e) => {
+    e.preventDefault();
+    this.setState({ showVideo: true });
+  }
 
-  const to = customerData.link;
-  const image = photos[customer];
-  const ContainerElement = to ? Link : 'div';
+  render() {
+    const customer = this.props.customer;
+    const customerData = customerCards[customer];
 
-  return (
-    <ContainerElement to={to} className={`${styles.container} ${to ? styles.linkedContainer : ''}`}>
-      {image &&
-        <div className={styles.imageContainer}>
-        {customerData.videoLength && 
-            <div className={styles.videoLength}>
-              <div className={styles.videoPlayIcon}>
-                <img src={videoPlayIcon} alt="Video" />
-              </div>
-              <p className="L-bold">{customerData.videoLength}</p>
+    const to = customerData.link;
+    const image = photos[customer];
+    const ContainerElement = to ? Link : 'div';
+
+    return (
+      <ContainerElement to={to} className={`${styles.container} ${to ? styles.linkedContainer : ''}`}>
+        {image &&
+          <div className={styles.imageContainer}>
+            {customerData.videoId &&
+              <React.Fragment>
+                {this.state.showVideo &&
+                  <div className={styles.videoContainer}>
+                    <WistiaVideo videoId={customerData.videoId} autoPlay="true" />
+                  </div>
+                }
+
+                {!this.state.showVideo &&
+                  <React.Fragment>
+                    <div className={styles.videoLength}>
+                      <div className={styles.videoPlayIcon}>
+                        <img src={videoPlayIcon} alt="Video" />
+                      </div>
+                      <p className="L-bold">{customerData.videoLength}</p>
+                    </div>
+                    <div className={styles.videoClick} onClick={this.videoClick}></div>
+                  </React.Fragment>
+                }
+              </React.Fragment>
+            }
+
+            {!this.state.showVideo &&
+              <img src={image} className={styles.image} alt="Customer" />
+            }
+          </div>
+        }
+
+        <div className={styles.card}>
+          <div className={`${styles.arrow} ${image ? styles.withImage : ''}`}><Arrow /></div>
+
+          {logos[customer] &&
+            <img src={logos[customer]} className={styles.logo} alt="Customer logo" />
+          }
+
+          {customerData.headline &&
+            <h5 dangerouslySetInnerHTML={{ __html: customerData.headline }}></h5>
+          }
+
+          {customerData.text &&
+            <p>{customerData.text}</p>
+          }
+
+          {customerData.author &&
+            <p className={styles.author}>{customerData.author}</p>
+          }
+
+          {to &&
+            <div className={styles.readMore}>
+              <p>Read More</p>
             </div>
           }
-          <img src={image} className={styles.image} alt="Customer" />
+
+          {customerData.tags &&
+            <div className={styles.tags}>
+              {customerData.tags.map((tag, idx) => {
+                return <span key={idx} className={styles.tag}>{tag}</span>
+              })}
+            </div>
+          }
+
         </div>
-      }
+      </ContainerElement>
+    );
+  }
+}
 
-      <div className={styles.card}>
-        <div className={`${styles.arrow} ${image ? styles.withImage: ''}`}><Arrow /></div>
 
-        {logos[customer] &&
-          <img src={logos[customer]} className={styles.logo} alt="Customer logo" />
-        }
-
-        {customerData.headline &&
-          <h5 dangerouslySetInnerHTML={{ __html: customerData.headline }}></h5>
-        }
-
-        {customerData.text &&
-          <p>{customerData.text}</p>
-        }
-
-        {customerData.author &&
-          <p className={styles.author}>{customerData.author}</p>
-        }
-
-        {to &&
-          <div className={styles.readMore}>
-            <p>Read More</p>
-          </div>
-        }
-
-        {customerData.tags &&
-          <div className={styles.tags}>
-            {customerData.tags.map((tag, idx) => {
-              return <span key={idx} className={styles.tag}>{tag}</span>
-            })}
-          </div>
-        }
-
-      </div>
-    </ContainerElement>
-  )
-};
+export default Card;
