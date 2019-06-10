caa6e3c3c1139843cce0268787bd4525d918d3a5
mjp <peterson514@gmail.com>
2019-06-10 13:39:12 -0400

    Favicon & blog share image

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,6 +44,36 @@ module.exports = {
         verboseOutput: true,
       },
     },
-    `gatsby-plugin-react-helmet`
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/images/favicon.png',
+
+        appName: 'aptible.com',
+        appDescription: null,
+        developerName: null,
+        developerURL: null,
+        dir: 'auto',
+        lang: 'en-US',
+        background: '#fff',
+        theme_color: '#fff',
+        display: 'standalone',
+        orientation: 'any',
+        start_url: '/?homescreen=1',
+        version: '1.0',
+
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          yandex: false,
+          windows: false
+        }
+      }
+    }
   ],
 };
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,12 +3,17 @@ import { Helmet } from 'react-helmet';
 import { graphql } from 'gatsby';
 import AptibleLayout from '../components/layouts/AptibleLayout';
 import Post from '../components/blog/Post';
+import shareImage from '../images/launch-post-visual.gif';
 
 export default ({ data }) => (
   <AptibleLayout>
     <Helmet>
       <title>Aptible | {data.post.title}</title>
       <meta name="description" content={data.post.title} />
+      <meta property="og:title" content={data.post.title} />
+      <meta property="og:description" content={`Aptible Blog: ${data.post.title}`} />
+      <meta property="og:image" content={shareImage} />
+      <meta name="twitter:card" content="summary_large_image" />
     </Helmet>
     <Post post={data.post} />
   </AptibleLayout>
