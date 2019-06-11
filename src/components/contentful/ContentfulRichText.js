7e91b968060043e0bc842bee96171cdb79cb9e1c
Skylar Anderson <skylar@aptible.com>
2019-06-10 21:39:18 -0700

    render asset blocks

--- src/components/contentful/ContentfulRichText.js
+++ src/components/contentful/ContentfulRichText.js
@@ -1,5 +1,18 @@
+import React from 'react';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import styles from './RichText.module.css';
+
+const renderAssetBlock = (node) => {
+  const { title, description, file } = node.data.target.fields;
+  return <img class={styles.image} src={file['en-US'].url} />
+}
+
+const options = {
+  renderNode: {
+    'embedded-asset-block': renderAssetBlock
+  }
+}
 
 export default ({ json }) => {
-  return documentToReactComponents(json);
+  return documentToReactComponents(json, options);
 };
