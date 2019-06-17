04da96525547a9d1911464f73a18e8e42e44ea85
mjp <peterson514@gmail.com>
2019-06-17 09:19:26 -0400

    Blog post video embed

--- src/components/contentful/ContentfulRichText.js
+++ src/components/contentful/ContentfulRichText.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import WistiaVideo from '../shared/WistiaVideo';
 import styles from './RichText.module.css';
 
 const renderAssetBlock = (node) => {
@@ -7,9 +8,26 @@ const renderAssetBlock = (node) => {
   return <img className={styles.image} src={file['en-US'].url} alt="contentful" />
 }
 
+const renderWistiaBlockquote = (node, children) => {
+  if (node.content.length === 1 && node.content[0].content.length === 1) {
+    if (node.content[0].content[0].value.startsWith('wistia:')) {
+      const videoId = node.content[0].content[0].value.replace('wistia:', '');
+
+      return (
+        <div className={styles.video}>
+          <WistiaVideo videoId={videoId} />
+        </div>
+      )
+    }
+  }
+
+  return <blockquote>{children}</blockquote>;
+}
+
 const options = {
   renderNode: {
-    'embedded-asset-block': renderAssetBlock
+    'embedded-asset-block': renderAssetBlock,
+    'blockquote': renderWistiaBlockquote
   }
 }
 
