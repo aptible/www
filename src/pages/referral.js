da0838c97efdc744a107396d844ab30eae5647bb
mjp <peterson514@gmail.com>
2019-07-02 09:35:58 -0400

    Referral page

---
+++ src/pages/referral.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import AptibleLayout from '../components/layouts/AptibleLayout';
+import Hero from '../components/referral/Hero';
+
+export default () => (
+  <AptibleLayout>
+    <Helmet>
+      <title>Aptible Referral Program</title>
+    </Helmet>
+    
+    <Hero />
+  </AptibleLayout>
+);
