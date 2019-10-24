8cadaa6187634d30192ffb6a4704466bb318e2e9
mjp <peterson514@gmail.com>
2019-10-24 14:18:43 -0400

    Link to security management guide

--- src/components/home/HeroCallout.js
+++ src/components/home/HeroCallout.js
@@ -4,9 +4,9 @@ import styles from './HeroCallout.module.css';
 
 export default () => (
   <div className={styles.callout}>
-    <Link to="/blog/introducing-new-brand-identity/">
-      <span className={styles.pill}>New</span>
-      <span className={styles.text}>Introducing our new brand</span>
+    <Link to="/security-management/">
+      <span className={styles.pill}>Free</span>
+      <span className={styles.text}>Security Management Guide</span>
       <span className={styles.arrow}>&rarr;</span>
     </Link>
   </div>
