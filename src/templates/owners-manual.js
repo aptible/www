b6aa53a1a0a8874ecf92cd8d9a0502021487004c
mjp <peterson514@gmail.com>
2020-01-15 10:14:02 -0500

    Aptible Owners Manual (#14)
    * Initial templates
    
    * Sort owners manual pages, add displayTitle field
    
    * Update CustomerCarousel.js
    
    Remove unused import
    
    * Adjust CSS for owners manual pages
    
    * Filter out owners manual entries that aren't ready to be seen
    
    Co-authored-by: Tom Cunningham <thomasthesecond@users.noreply.github.com>

---
+++ src/templates/owners-manual.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import AptibleLayout from '../components/layouts/AptibleLayout';
+import Page from '../components/owners-manual/Page';
+
+const getCurrentPage = (activePath, allPages) => {
+  const pageEdge = allPages.find(e => e.node.slug === activePath);
+  return pageEdge.node;
+}
+
+export default ({ pageContext }) => {
+  console.log(pageContext);
+  const currentPage = getCurrentPage(pageContext.activePath, pageContext.allPages);
+
+  return (
+    <AptibleLayout>
+      <Helmet>
+        <title>{currentPage.displayTitle || currentPage.title} | Aptible Owners Manual</title>
+        <meta name="description" content="Meta description here." />
+      </Helmet>
+      <Page currentPage={currentPage} allPages={pageContext.allPages} />
+    </AptibleLayout>
+  );
+};
