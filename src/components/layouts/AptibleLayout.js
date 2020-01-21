d2155e41e54be8ad58404fa8eded27b579b75d0f
Tom Cunningham <thomasthesecond@users.noreply.github.com>
2020-01-21 10:29:27 -0500

    Add HelloBar for security management blog post (#30)

--- src/components/layouts/AptibleLayout.js
+++ src/components/layouts/AptibleLayout.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
+import HelloBar from '../shared/HelloBar';
 import CookieConsent from '../shared/CookieConsent';
 import Analytics from '../shared/Analytics';
 
 export default ({ children }) => (
   <div>
+    <HelloBar
+      id="blog-what-is-security-management"
+      to="/blog/what-is-security-management/"
+      callout="New Blog Post"
+      text="Intro to Security Management: Learn about about Security Management, its importance, and how it can benefit your business."
+    />
     <Header />
     <CookieConsent />
     {children}
