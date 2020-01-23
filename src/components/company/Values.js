0cd881565b99c9e57599cb7f1cb3def30b327ccd
Tom Cunningham <tom.cunningham@aptible.com>
2020-01-23 15:25:34 -0500

    Update values

--- src/components/company/Values.js
+++ src/components/company/Values.js
@@ -4,77 +4,73 @@ import styles from './Values.module.css';
 
 const items = [
   {
-    title: 'Commitment to the Team',
-    content: <p className="L">
-      We're passionate about building a diverse team of talented people who
-      accomplish great things together, regardless of where they are. We talk
-      a lot about the science of high-performing teams and how to make Aptible
-      the best environment to support that kind of team. If you love the mutual
-      support and camaraderie of a strong team that wins together, we want to
-      hear from you.
-    </p>
+    title: 'Done is Better than Perfect',
+    content: (
+      <>
+        <p className="L">“The perfect is the enemy of the good.” ― Voltaire</p>
+        <p className="L">
+          We identify the core value of a project and ship the simple thing
+          first. We’re willing to set a goal we’re not sure of because it
+          inspires us to act in the spirit of learning, reflect on the results,
+          and gather valuable data about what to try next. We fail often and
+          early in pursuit of experimentation and risk-taking on novel ideas.
+        </p>
+        <p className="L">
+          We value and respect teaching as the best way to learn, and make time
+          to share information openly. We are a team of learners with a growth
+          mindset: We talk about improving constantly and challenge each other
+          to grow.
+        </p>
+      </>
+    ),
   },
   {
-    title: 'Commitment to Each Other',
-    content: <p className="L">
-      We commit to each other as individuals, one on one. We measure our
-      commitments carefully and hold each other accountable, which means we’re
-      able to deliver more for our colleagues and customers in the long run. We
-      set only a few very clear, ambitious goals as a company each quarter, and
-      talk about them constantly to reinforce focus. We make time to recognize,
-      praise, and reward those who consistently deliver on their promises.
-    </p>
+    title: 'Be Hard on Problems, Not People',
+    content: (
+      <>
+        <p className="L">
+          “Feedback comes from a place of positive intent.” —{' '}
+          <a href="https://medium.com/@michaelterrell/what-is-feedback-anyways-2a803c7f2d59">
+            Michael Terrell
+          </a>
+        </p>
+        <p className="L">
+          We discuss our questions and concerns openly as a group. We practice
+          giving effective feedback, with the intent of helping each other be
+          more successful. When we give feedback, we make each other feel big,
+          not small.
+        </p>
+        <p className="L">
+          We work to create an environment where it feels completely safe to ask
+          for help and take a risk — where we feel valued for our contributions
+          and accepted for who we are. We don't attribute mistakes or unexpected
+          outcomes to human error: We view them as a starting point for an
+          inquiry, not the end. We can resolve difficult issues while supporting
+          each other.
+        </p>
+      </>
+    ),
   },
   {
-    title: 'Commitment to The Mission',
-    content: <p className="L">
-      Startups are hard, and we expect challenges and missteps. We love winning,
-      but most of all, we value getting back up and getting better at what we
-      do. We are deeply motivated to build trust on the internet by empowering
-      teams, and we respect grit and perseverance in furtherance of that mission.
-    </p>
-  },
-  {
-    title: 'Growth Over Perfection',
-    content: <p className="L">
-      We fail often and early, and learn from it. We talk about improving
-      constantly. We praise those who give feedback early and often. We put
-      effort into being good at giving feedback: When we give feedback, we make
-      each other feel big, not small. This helps create an environment where we
-      can take reasonable risks, which in turn helps us make decisions quickly
-      and learn fast. We expect managers to delegate a lot, and acknowledge that
-      delegation means sometimes letting someone make what you think is the
-      wrong decision.
-    </p>
-  },
-  {
-    title: 'Teaching as Learning',
-    content: <p className="L">
-      We are a team of learners. We value and respect teaching as
-      the best way to learn, and make time to share information openly. We set
-      explicit goals around training, and devote resources to developing and
-      improving our internal training. We praise those who share knowledge in
-      public by asking and answering questions in team chat, writing
-      documentation, and writing for our customers.
-    </p>
-  },
-  {
-    title: 'Taking Responsibility Over Giving Responsibility',
-    content: <p className="L">
-      Identifying problems is a good start, but we love solving them more. We
-      think and talk a lot about how we can improve, and work hard at it. We
-      value and reward those who take initiative. We don't like meetings where
-      we only talk about problems.
-    </p>
-  },
-  {
-    title: 'Asking Why',
-    content: <p className="L">
-      We ask “why?” a lot in order to understand root causes. Asking why helps
-      us cultivate focus and make high-quality decisions quickly. We don't
-      attribute mistakes to human error. We view them as a starting point for
-      an inquiry, not the end.
-    </p>
+    title: 'Ask Why',
+    content: (
+      <>
+        <p className="L">
+          “Is the juice worth the squeeze?” — Source Unknown
+        </p>
+        <p className="L">
+          We seek to understand before we act. We question our assumptions and
+          surface differences of opinion, in order to discover gaps in our plans
+          and make changes accordingly.
+        </p>
+        <p className="L">
+          By asking “Why?”, we cultivate focus and create leverage: We pick the
+          right things to work on and maximize the impact of our efforts. By
+          asking “Why?”, we make space for the creative tension between optimism
+          and skepticism, and think strategically about what and how we build.
+        </p>
+      </>
+    ),
   },
 ];
 
@@ -84,6 +80,7 @@ export default () => (
       title="Our Values"
       linkText="See Careers at Aptible"
       linkTo="/careers/"
-      items={items} />
+      items={items}
+    />
   </div>
 );
