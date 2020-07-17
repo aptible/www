062a758264face93a8748d8d5b694e7dfac3bac2
Tom Cunningham <tom.cunningham@aptible.com>
2020-07-17 11:58:07 -0400

    Create shared Footnotes component

---
+++ src/components/shared/Footnotes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Footnotes.module.css';
+
+export const FootnoteMarker = ({ children }) => (
+  <sup className={styles.footnoteMarker}>{children}</sup>
+)
+
+const Footnotes = ({ footnotes }) => {
+  return footnotes.map(footnote => (
+    <div className={styles.footnotes} key={footnote.marker}>
+      <FootnoteMarker>{footnote.marker}</FootnoteMarker>
+      <div>{footnote.note}</div>
+    </div>
+  ));
+};
+
+export default Footnotes;
