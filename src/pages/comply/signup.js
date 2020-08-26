c6574957ba69039975971df5de1db3244e2420d6
Tom Cunningham <tom.cunningham@aptible.com>
2020-08-26 14:47:06 -0400

    Move Comply sign up page

--- src/pages/comply/signup.js
+++ src/pages/comply/signup.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import AptibleLayout from '../components/layouts/AptibleLayout';
-import SignupPage from '../components/signup/SignupPage';
+import AptibleLayout from '../../components/layouts/AptibleLayout';
+import SignupPage from '../../components/signup/SignupPage';
 
 export default () => (
   <AptibleLayout>
     <Helmet>
-      <title>Aptible | Signup</title>
+      <title>Aptible | Sign Up for Aptible Comply</title>
       <meta name="description" content="Aptible is the leading security management platform for small companies. Grow by meeting security requirements for SOC 2, ISO 27001, HIPAA, GDPR, and more." />
     </Helmet>
     <SignupPage product="comply" />
