80d2d5e329ab4e1818758b76cfe0a67a1011e61a
Tom Cunningham <tom.cunningham@aptible.com>
2020-08-26 14:49:57 -0400

    Update Sign Up page

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -6,7 +6,7 @@ import SignupPage from '../components/signup/SignupPage';
 export default () => (
   <AptibleLayout>
     <Helmet>
-      <title>Aptible | Signup</title>
+      <title>Aptible | Sign Up</title>
       <meta name="description" content="Aptible is the leading security management platform for small companies. Grow by meeting security requirements for SOC 2, ISO 27001, HIPAA, GDPR, and more." />
     </Helmet>
     <SignupPage />
