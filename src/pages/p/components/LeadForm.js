76df6613d3ed4854edf2d389b3aa05b575e90eb1
Skylar Anderson <skylar@aptible.com>
2021-07-13 11:32:36 -0700

    show thank you message

--- src/pages/p/components/LeadForm.js
+++ src/pages/p/components/LeadForm.js
@@ -1,10 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import cn from 'classnames';
 import styles from './LeadForm.module.css';
 import buttonStyles from '../../../components/buttons/Button.module.css'
 
 export default ({ id }) => {
+  const [submitted, setSubmitted] = useState(false);
+
+  const onSubmit = () => {
+    setSubmitted(true) ;
+  }
+
   return (
     <div>
       <Helmet>
@@ -17,7 +23,7 @@ export default ({ id }) => {
       </Helmet>
 
       <div className={styles.leadFormContainer}>
-        <form id={id}>
+        <form id={id} onSubmit={onSubmit}>
           <input
             required
             className={styles.leadFormInput}
@@ -30,6 +36,10 @@ export default ({ id }) => {
           </button>
         </form>
       </div>
+
+      {submitted &&
+        <div className={styles.submissionNotification}>Thanks! We'll contact you for a Demo shortly. </div>
+      }
     </div>
   );
 };
