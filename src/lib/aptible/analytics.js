aa240372a1cb10c2a016a5d77272b5dfababb8f6
Skylar Anderson <skylar@aptible.com>
2021-07-14 15:20:09 -0700

    add linked in tracking (#181)

--- src/lib/aptible/analytics.js
+++ src/lib/aptible/analytics.js
@@ -54,6 +54,11 @@ export function currentURL() {
   return `${window.location.origin}${window.location.pathname}`;
 }
 
+export function trackOnLinkedIn(pid='42067', conversionId='2213244') {
+  new Image().src =
+    `https://px.ads.linkedin.com/collect/?pid=${pid}&conversionId=${conversionId}&fmt=gif`
+}
+
 export function event(name, payload = {}) {
   if (window.analytics) {
     payload = Object.assign({}, payload, allUtmVars());
