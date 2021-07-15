b38a1b2ee813194c91644726edb4bdcb7b16e269
Eric Bower <me@erock.io>
2021-07-15 13:20:15 -0400

    redirect properly

--- src/components/header/Resources.js
+++ src/components/header/Resources.js
@@ -49,7 +49,7 @@ const Resources = () => (
       </div>
 
       <div className={styles.calloutContainer}>
-        <Link className={styles.callout} to="/hipaa/">
+        <Link className={styles.callout} to="/hipaa-guide-form/">
           <img src={pic} alt="" />
           <div className={styles.calloutText}>
             <p>Aptible Guide</p>
