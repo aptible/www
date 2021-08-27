b342eb17a546bd333be394e9fb9a83479c8770f0
Ashley Mathew <ashley@aptible.com>
2021-08-27 15:58:12 -0400

    Track pageviews

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,8 @@ import './src/styles/search.css';
 import './src/styles/tables.css';
 
 export function onRouteUpdate({ location, prevLocation }) {
+  window.aptible.event('pageview', null);
+
   // Re-send Rollworks pixel on pushstate navigation changes
   if (prevLocation && window.__adroll) {
     window.__adroll.record_user({});
