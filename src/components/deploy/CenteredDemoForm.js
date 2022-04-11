112fe5511096f0d3f4dff3c765377f9aeb9dbcb9
Michael Peterson <peterson514@gmail.com>
2022-04-11 19:30:47 -0400

    Copy of homepage as a paid LP (#267)

--- src/components/deploy/CenteredDemoForm.js
+++ src/components/deploy/CenteredDemoForm.js
@@ -3,12 +3,12 @@ import styles from './CenteredDemoForm.module.css';
 
 import SignupForm from '../signup-form';
 
-export default ({ title, body, formId }) => (
+export default ({ title, body, formId, allowPersonalEmails = true }) => (
   <div className={styles.container}>
     <h2>{title}</h2>
     <p className="L">{body}</p>
     <div className={styles.leadFormContainer}>
-      <SignupForm id={formId} />
+      <SignupForm id={formId} allowPersonalEmails={allowPersonalEmails} />
     </div>
   </div>
 );
