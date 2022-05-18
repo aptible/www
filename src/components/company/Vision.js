88321a491f722e84b308a2b06b6de971fe6e2f88
Michael Peterson <peterson514@gmail.com>
2022-05-18 19:39:43 -0400

    Company page content updates

--- src/components/company/Vision.js
+++ src/components/company/Vision.js
@@ -11,7 +11,10 @@ export default () => (
           Aptible helps developers do their best work by empowering them to use any cloud infrastructure securely.
         </p>
         <p className="XL">
-          We intend to deliver the best way for developers to provision, monitor, and test any resource in any cloud environment to ensure their infrastructure always meets rigorous security and compliance requirements.
+          We seek to guarantee security and compliance by abstracting complex infrastructure decisions away from software developers.
+        </p>
+        <p className="XL">
+          First, we plan to make our platform easy enough that it is compelling for startups with existential compliance needs, and then we will make it flexible enough so that later stage companies build on Aptible instead of building an internal developer platform from scratch.
         </p>
       </div>
     </Grid>
