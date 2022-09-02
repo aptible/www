07d865bd4b1ba8fc8b08e6a1491094b076fa2ada
Michael Peterson <peterson514@gmail.com>
2022-09-02 16:19:17 -0400

    Add logos to customers page

--- src/components/customers/GreatCompany.js
+++ src/components/customers/GreatCompany.js
@@ -2,6 +2,33 @@ import React from 'react';
 import { Grid } from '../grid/Grid';
 import styles from './GreatCompany.module.css';
 
+import healthCatalystLogo from '../../images/customers/logos/health-catalyst.png';
+import ableHealthLogo from '../../images/customers/logos/able-health.png';
+import healthifyLogo from '../../images/customers/logos/healthify.svg';
+import mdsaveLogo from '../../images/customers/logos/mdsave.svg';
+import pathaiLogo from '../../images/customers/logos/pathai.png';
+import medicalMemoryLogo from '../../images/customers/logos/medical-memory-logo.png';
+import pagerLogo from '../../images/customers/logos/pager-logo.png';
+import vidscripLogo from '../../images/customers/logos/vidscip-logo.png';
+import rightwayLogo from '../../images/customers/logos/rightway-logo.png';
+import aidinLogo from '../../images/customers/logos/aidin-logo.png';
+import glidianLogo from '../../images/customers/logos/glidian-logo.png';
+import projectBeaconLogo from '../../images/customers/logos/project-beacon-logo.png';
+import himsHersLogo from '../../images/customers/logos/hims-hers-logo.png';
+import careportLogo from '../../images/customers/logos/careport-logo.png';
+import dorsataLogo from '../../images/customers/logos/dorsata.png';
+import greenspaceLogo from '../../images/customers/logos/greenspace.png';
+import stellarLogo from '../../images/customers/logos/stellar-health.png';
+import aliceLogoLogo from '../../images/customers/logos/alice-logo.png';
+import canvasLogo from '../../images/customers/logos/canvas-logo.png';
+import workItLogo from '../../images/customers/logos/workit-logo.png';
+
+const CustomerLogo = ({logo}) => (
+  <div className={styles.logoContainer}>
+    <img src={logo} />
+  </div>
+)
+
 export default () => (
   <div className={styles.container}>
     <Grid>
@@ -11,6 +38,28 @@ export default () => (
           We help hundreds of the most innovative companies to protect sensitive
           data when the stakes matters most.
         </p>
+      </div>
+
+      <div className={styles.logos}>
+        <CustomerLogo logo={canvasLogo} />
+        <CustomerLogo logo={aliceLogoLogo} />
+        <CustomerLogo logo={workItLogo} />
+        <CustomerLogo logo={himsHersLogo} />
+        <CustomerLogo logo={projectBeaconLogo} />
+        <CustomerLogo logo={stellarLogo} />
+        <CustomerLogo logo={greenspaceLogo} />
+        <CustomerLogo logo={dorsataLogo} />
+        <CustomerLogo logo={careportLogo} />
+        <CustomerLogo logo={glidianLogo} />
+        <CustomerLogo logo={aidinLogo} />
+        <CustomerLogo logo={rightwayLogo} />
+        <CustomerLogo logo={pagerLogo} />
+        <CustomerLogo logo={medicalMemoryLogo} />
+        <CustomerLogo logo={pathaiLogo} />
+        <CustomerLogo logo={mdsaveLogo} />
+        <CustomerLogo logo={healthifyLogo} />
+        <CustomerLogo logo={ableHealthLogo} />
+        <CustomerLogo logo={healthCatalystLogo} />
       </div>
     </Grid>
   </div>
